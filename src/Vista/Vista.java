08d70603104dd588f4fdf7d1882f25e408753c35
adrianbueno <a.bueno2@sapalomera.cat>
2024-05-15 13:45:39 +0200

    Commit prueba

---
+++ src/Vista/Vista.java
@@ -0,0 +1,14 @@
+package Vista;
+
+public class Vista {
+    public void mostrarDades(){
+        System.out.println("AAAAAAAAAAAAAAAAAAAHHHHHHHHHHHH");
+    }
+    public void Llistar_partits_jugats(){
+        System.out.println("Los Angeles Lakers – Seattle Supersonics: 114 – 92\n" +
+                "\n" +
+                "Los Angeles Lakers: 114\n" +
+                "Seattle Supersonics: 92");
+    }
+
+}
